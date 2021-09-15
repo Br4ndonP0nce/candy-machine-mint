0adcb9a8ca299d518ba4d407901aba281e1d94c5
Br4ndonP0nce <brandoncompany876@gmail.com>
2021-09-15 01:50:22 -0500

    started new branch

--- src/Home.tsx
+++ src/Home.tsx
@@ -163,51 +163,93 @@ const Home = (props: HomeProps) => {
     
       <main>
         
-        
-            <div className="header-2">
+        <div className="relative bg-white overflow-hidden">
+  <div className="max-w-7xl mx-auto">
+    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+        <polygon points="50,0 100,0 50,100 0,100" />
+      </svg>
 
-    <header className=" py-2 md:py-2 ">
-      <div className="flex items-center justify-between py-3 px-5 md:px-10 ">
+      <div>
+        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+            <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+              <div className="flex items-center justify-between w-full md:w-auto">
+                <a href="#">
+                  <span className="sr-only">Workflow</span>
+                  <img className="h-8 w-auto sm:h-10" src=""></img>
+                </a>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    
+                    
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+              <a href="#" className="font-medium text-gray-500 hover:text-gray-900">ASS</a>
 
-        <div className="flex items-center gap-2">
-        <img className='w-20'src="/like-this.png" alt="mainIcon" />
-          <a href="https://deadass.io/" className="font-bold text-xl text-indigo-600 px-5">DeadAss SOL</a>
-          
-          <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
-            <i className="fas fa-bars"></i>
-          </button>
+              <a href="#" className="font-medium text-gray-500 hover:text-gray-900">ASS</a>
+
+              <a href="#" className="font-medium text-gray-500 hover:text-gray-900">ASS</a>
+
+              <a href="#" className="font-medium text-gray-500 hover:text-gray-900">ASS</a>
+              
+              
+              
+
+              
+            </div>
+          </nav>
         </div>
 
-        <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-          <a href="https://twitter.com/deadasssol" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">
-          <img src="/icons/twitter-logoBlue.png" alt="" height="30" width="30"/>
-          </a>
-          <a href="https://discord.gg/Tc9P6JDX" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"><img src="/icons/discord_logo.png" alt="" height="25" width="25"/></a>
-          
-          
+       
+        <div className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img className="h-8 w-auto" src="" alt=""></img>
+              </div>
+              <div className="-mr-2">
+                <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close main menu</span>
+                  
+                  <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
+
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
+
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
+
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
+            </div>
+            <a href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
+              Log in
+            </a>
+          </div>
         </div>
       </div>
-    </header>
-    </div>
 
-
-<div className="flex flex-col items-center bg-gradient-to-r from-yellow-400 to-pink-400">
-  <div className="flex flex-row justify-between flex-wrap gap-5">
-  <main className="mt-10 mb-6 home-container flex flex-col items-center">
-    <p className="mb-4 text-2xl text-center font-bold text-black text-opacity-90">3 SOL/mint</p>
-    <p className="text-lg font-bold text-black text-opacity-90 mb-4">Grab yourself a piece of dat cake</p>
-    <div className="imageCointainer">
-      <img src="./stronkAss.gif" alt="" width="450" className="gif" />
-    </div>
-    <div className= "py-20 px-20 height-40">
-    {wallet.connected && (
-        <div className="flex items-center">
-        <p>Connected with {shortenAddress(wallet.publicKey?.toBase58() || "")}</p>
-        </div>
-      )}
-
-      
-      <MintContainer>
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="sm:text-center lg:text-left">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">NFT cake</span>
+            <span className="block text-indigo-600 xl:inline">Deadass Tho</span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            The First Ass in Defi
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+            <MintContainer>
         {!wallet.connected ? (
           <ConnectButton >Connect Wallet</ConnectButton>
         ) : (
@@ -238,19 +280,49 @@ const Home = (props: HomeProps) => {
           
         )}
       </MintContainer>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                Read more
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <img className="h-56 w-full object-cover sm:h-56 md:h-56 lg:w-full lg:h-full" src="./stronkAss.gif" alt="">
+    </img>
+  </div>
+</div>
+            <div className="header-2">
+
+    <header className=" ">
+      <div className="flex bg-blue-100 items-center justify-between py-3 px-5 md:px-10 ">
+
+        <div className="flex items-center gap-2">
+          <span className="block text-indigo-600 xl:inline px-3">DeadAss SOL</span>
+          <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+
+        <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+          <a href="https://twitter.com/deadasssol" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">
+          <img src="/icons/twitter-logoBlue.png" alt="" height="30" width="30"/>
+          </a>
+          <a href="https://discord.gg/Tc9P6JDX" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"><img src="/icons/discord_logo.png" alt="" height="25" width="25"/></a>
+          
+          
+        </div>
       </div>
+    </header>
+    </div>
 
-</main>
-<div className=" home-container flex flex-col items-center justify-center">
-    <img src="/logo.png" alt="Deadass logo" width="300"/>
-              
-    
-    
-   
 
-</div>
+<div className="flex flex-col items-center bg-gradient-to-r from-yellow-400 to-pink-400">
 
-</div>
 </div>
 <section className="bg-gradient-to-r from-pink-400 to-yellow-400 h-80 flex flex-col items-center">
   <main className="mt-10 mb-6 home-container flex flex-col items-center">
